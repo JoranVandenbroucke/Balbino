8a91e3bbc40d090950743e2c90bda0f285df1d6b
Joran Vandenbroucke <joran.vandenbroucke@student.howest.be>
2020-03-20 11:51:04 +0100

    seperation

--- Minigin/Struct.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-struct Color
-{
-	Color( unsigned char r, unsigned char g, unsigned char b, unsigned char a = 255 );
-	Color( uint32_t color );
-
-	unsigned char r, g, b, a;
-};
