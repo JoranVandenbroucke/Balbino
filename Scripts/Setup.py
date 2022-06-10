3807bfbb18fbd93c5c0460ef9eb2166845b43a10
Vandenbroucke Joran <47856973+JoranVandenbroucke@users.noreply.github.com>
2022-06-10 17:08:28 +0200

    Update and rename SetupVulkan.py to Setup.py

--- Scripts/Setup.py
+++ Scripts/Setup.py
@@ -3,3 +3,5 @@ from zipfile import ZipFile
 with ZipFile('../3rdParty/Vulkan.zip', 'r') as zipObj:
    # Extract all the contents of zip file in different directory
    zipObj.extractall('../3rdParty')
+   
+start ../3rdParty/assimp/BUILDBINARIES_EXAMPLE.bat
